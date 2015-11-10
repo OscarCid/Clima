be245286b6d239f034108dd6e4cc6b0f0b6bc030
OscarCid <oscarcidibaceta@gmail.com>
2015-11-09 21:15:27 -0300

    Creacion archivos Widget

--- scr/php/widget/widgetYali.php
+++ scr/php/widget/widgetYali.php
@@ -3,7 +3,7 @@
     mysql_connect("localhost","root",""); // host, username, password...
     mysql_select_db("clima"); // db name...
       
-    $q=mysql_query("SELECT * FROM yali ORDER BY ordenar DESC LIMIT 100");
+    $q=mysql_query("SELECT * FROM yali ORDER BY ordenar DESC LIMIT 1");
     while($row=mysql_fetch_assoc($q))
             $json_output[]=$row;
       
